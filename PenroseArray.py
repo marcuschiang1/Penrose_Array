a9d8fc0f7be51e81e17d741c555c3575795dd8b6
Marcus Chiang <19mhzc@queensu.ca>
2024-03-26 22:52:52 -0400

    binary search

--- PenroseArray.py
+++ PenroseArray.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
+import subprocess
 class PenroseArray:
     def __init__(self, array):
         self.array = array
 
-    def generate(self, file_name):
+    def generate_substance(self, file_name):
         file_path = os.path.abspath(file_name)
         # if not os.path.isfile(file):
         #     raise FileNotFoundError
@@ -12,14 +13,33 @@ class PenroseArray:
         initial_variables = ["Array a_1 \n",
                              "Element e_i for i in [0,"+str(length)+"] \n",
                              "Index i_j for j in [0,"+str(length)+"] \n",
-                             "IndexOf(e_j, i_j, j) for j in [0,"+str(length)+"] \n",
-                             "In(e_j, a_1) for i in [0,"+str(length)+"] \n"]
+                             "IndexOf(e_j, i_j) for j in [0,"+str(length)+"] \n",
+                             "In(e_i, a_1) for i in [0,"+str(length)+"] \n"]
         f = open(file_name, "w")
         f.writelines(initial_variables)
         i_labels, e_labels = self.create_labels()
         f.writelines(i_labels)
         f.writelines(e_labels)
-    
+        f.close()
+
+    def binary_search(self, file_name):
+        self.generate_substance(file_name)
+        length = len(self.array)
+        f = open(file_name, "a")
+        middle = length/2
+        #Even length array
+        if length%2 == 0:
+            
+            f.writelines([
+                "Middle(e_"+str(int(middle-1))+","+"i_"+str(int(middle-1))+")\n",
+                "Middle(e_"+str(int(middle))+","+"i_"+str(int(middle))+")"
+            ])
+        #Odd length array
+        else:
+            f.write("Middle(e_"+str(int(middle-.5))+","+"i_"+str(int(middle-.5))+")")
+        f.close()
+
+
     def create_labels(self):
         i_labels = [None]*len(self.array)
         e_labels = [None]*len(self.array)
@@ -31,9 +51,9 @@ class PenroseArray:
         return i_labels, e_labels    
         
 def main():
-    array = [10,"hello","wdd",50000,50,60,70,80]
+    array = [1,"hi",3, 4]
     example = PenroseArray(array)
-    example.generate("test2.substance")
+    example.binary_search("binary_search.substance")
     print(array)
     
         
--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-import { compile, optimize, toSVG, showError } from "@penrose/core";
+import { fetchResolver } from "@penrose/components";
+import { diagram } from "@penrose/core";
 import trio from "./trio.js";
 
-const compiled = await compile(trio);
-// handle compilation errors
-if (compiled.isErr()) {
-  throw new Error(showError(compiled.error));
-}
-const converged = optimize(compiled.value);
-// handle optimization errors
-if (converged.isErr()) {
-  throw new Error(showError(converged.error));
-}
-// render the diagram state as an SVG
-const rendered = await toSVG(converged.value, async () => undefined);
-const container = document.getElementById("diagram");
-container.appendChild(rendered);
+await diagram(trio, document.getElementById("diagram"), fetchResolver);
