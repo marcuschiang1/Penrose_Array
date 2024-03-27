4f5c88861dad6442fca9e9fb92a159fc1cb29c0c
Marcus Chiang <19mhzc@queensu.ca>
2024-03-27 18:57:50 -0400

    Taking out the trash

--- script.js
+++
@@ -1,5 +0,0 @@
-import { fetchResolver } from "@penrose/components";
-import { diagram } from "@penrose/core";
-import trio from "./trio.js";
-
-await diagram(trio, document.getElementById("diagram"), fetchResolver);
